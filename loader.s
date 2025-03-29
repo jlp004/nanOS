6ad1df2168d034fd9a5ae0d6f374db8cf164cbe7
john <johnlphelan2004@gmail.com>
2025-03-29 00:46:36 -0500

    esp stack in loader

--- loader.s
+++ loader.s
@@ -3,6 +3,8 @@ MAGIC_NUMBER    equ 0x1BADB002
 FLAGS           equ 0x0
 CHECKSUM        equ -MAGIC_NUMBER
 
+KERNEL_STACK_SIZE equ 4096
+
 section .text
 align 4
     dd MAGIC_NUMBER
@@ -11,6 +13,12 @@ align 4
 
 
 loader:
-    mov eax, 0xCAFEBABE                     ; moving a value into eax to check logs to compare if kernel launched
+    mov esp, kernel_stack + KERNEL_STACK_SIZE   ; move esp pointer to the end of the reserved stack space
+    mov eax, 0xCAFEBABE                         ; moving a value into eax to check logs to compare if kernel launched
 .loop:
     jmp .loop
+
+section .bss
+align 4
+kernel_stack:
+    resb KERNEL_STACK_SIZE                      ; reserve stack space for the kernel    
